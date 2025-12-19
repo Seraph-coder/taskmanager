478dd160677f5edea8788f0cd4bd290693e4e0da
Marchenko Dmitriy <dimamarch2014@gmail.com>
2025-12-19 11:46:20 +0500

    Rebase commit

---
+++ src/main/java/ru/naujava/taskmanager/controller/command/ShowDoneCommand.java
@@ -0,0 +1,41 @@
+package ru.naujava.taskmanager.controller.command;
+
+import org.springframework.stereotype.Component;
+import ru.naujava.taskmanager.bot.BotConstants;
+import ru.naujava.taskmanager.controller.Action;
+import ru.naujava.taskmanager.controller.CommandResponse;
+import ru.naujava.taskmanager.service.TaskService;
+
+/**
+ * Команда для отображения списка выполненных задач пользователя.
+ *
+ * @author Seraph-coder
+ * @since 18.12.2025
+ */
+@Component
+public class ShowDoneCommand implements BotCommand {
+    private final TaskService taskService;
+
+    /**
+     * Конструктор команды показа выполненных задач.
+     */
+    public ShowDoneCommand(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @Override
+    public String getCommandName() {
+        return "/showdone";
+    }
+
+    @Override
+    public CommandResponse execute(String command, Long chatId) {
+        if (chatId == null) {
+            return new CommandResponse(
+                    BotConstants.MSG_UNKNOWN_USER,
+                    Action.NONE, null, true);
+        }
+        String taskList = taskService.formatCompletedTaskAsString(chatId);
+        return new CommandResponse(taskList, Action.NONE, null, true);
+    }
+}
